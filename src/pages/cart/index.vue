a89d25997de719202a615e4a7eefcfade20363a4
unknown <1060556811@qq.com>
2019-02-16 09:14:14 +0800

    yxm

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,31 +1,123 @@
 <template>
-  <div>
-    这是首页
+	<div>
+		<div class="product-list-li" v-for="(item,index) in product">
+			<!--选择框-->
+			<div class="product-left">
+
+			</div>
+    <!--内容-->
+			<div class="product-canter">
+				<div class="img">
+					<img :src="item.img" />
+				</div>
+				<div class="content">
+					<div class="tit">{{item.tit}}</div>
+					<div class="price">
+						<span>¥{{item.price}}</span>
+					</div>
+					<div class="price1" :style="{display:isflex}">
+						<span>¥{{item.price}}-</span>
+						<span>{{ticket}}</span>
+						<div class="ptq">平台卷</div>
+					</div>
+				</div>
+			</div>
+<!--加减-->
+  <div class="product-right">
+  	<span>-</span>
+  	<span>{{num}}</span>
+  	<span>+</span>
   </div>
+ 		</div>
+	</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				product: [{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
 
+					},
+					{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
+					},
+					{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
+					},
+					{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
+					},
+				],
+			}
+		},
 
-export default {
-  data () {
-    return {
-    
-    }
-  },
+		components: {},
 
-  components: {
-  },
+		methods: {
 
-  methods: {
-   
-  },
+		},
 
-  created () {
-   
-  }
-}
+		created() {
+
+		}
+	}
 </script>
 
-<style scoped>
+<style lang="less">
+	.product-canter{
+		width: 202px;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		.img {
+			width: 60px;
+			height: 60px;
+			img {
+				width: 100%;
+				height: 100%;
+			}
+		}
+		.content {
+			width: 143px;
+			font-size: 11px;
+			.tit {
+				color: #333333;
+				line-height: 18px;
+				margin-top: 10px;
+			}
+			.price {
+				margin-top: 20px;
+				span {
+					color: #a82429;
+					font-size: 16px;
+				}
+			}
+			.price1 {
+				color: #a82429;
+				font-size: 16px;
+				.ptq {
+					background-color: #801d20;
+					width: 36px;
+					height: 13px;
+					text-align: center;
+					border-radius: 5px;
+					line-height: 13px;
+					color: #ffffff;
+					font-size: 7px;
+					display: inline-block;
+					vertical-align: middle;
+				}
+			}
+		}
+	}
 </style>
--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -1,31 +1,223 @@
 <template>
-  <div>
-    这是首页
-  </div>
+	<div>
+		<!--轮播-->
+		<div class='test'>
+			<swiper display-multiple-items='1' circular previous-margin='28px' next-margin='28px' indicator-dots>
+				<div v-for="(item,index) in banner">
+					<swiper-item>
+						<div class='box'>
+							<img :src='item.banners'></img>
+						</div>
+					</swiper-item>
+				</div>
+			</swiper>
+		</div>
+		<!--商品类目-->
+		<div class="cate">
+			<div class="cate-li" v-for="(item,index) in cate">
+				<span><img :src="item.img"/></span>
+				<span>{{item.name}}</span>
+			</div>
+		</div>
+		<!--海报-->
+		<div class="poster">
+			<img :src="poster.img" />
+		</div>
+		<!--产品列表-->
+		<div class="product">
+			<div class="headline">商品列表 </div>
+			<div class="product-list">
+	           <product :products="product"></product>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import product from '@/components/product'
+	export default {
+		data() {
+			return {
+			    isflex:"none",
+				poster: {
+					img: "/static/images/banner.png"
+				},
+					ticket:20,  //平台券
+				product: [{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
 
+					},
+					{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
+					},
+					{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
+					},
+					{
+						img: "/static/images/product-list.png",
+						tit: "元淳孕妇爽肤水保湿水天然孕妇护肤品化妆品补水洋甘菊专用柔肤水",
+						price: "198"
+					},
+				],
+				cate: [{
+						img: "/static/images/product-cate.png",
+						name: "会员专属"
 
-export default {
-  data () {
-    return {
-    
-    }
-  },
+					},
+					{
+						img: "/static/images/product-cate.png",
+						name: "会员专属"
+					},
+					{
+						img: "/static/images/product-cate.png",
+						name: "会员专属"
+					},
+					{
+						img: "/static/images/product-cate.png",
+						name: "会员专属"
+					},
+				],
+				banner: [{
+						banners: "/static/images/banner.png"
+					},
+					{
+						banners: "/static/images/banner.png"
+					},
+					{
+						banners: "/static/images/banner.png"
+					},
+				]
+			}
+		},
 
-  components: {
-  },
+		components: {
+			product,
+		},
 
-  methods: {
-   
-  },
+		methods: {
 
-  created () {
-   
-  }
-}
+		},
+
+		created() {
+
+		}
+	}
 </script>
 
-<style scoped>
+<style lang="less">
+	/*轮播*/
+	
+	.test {
+		width: 100%;
+		height: 172px;
+		box-sizing: border-box;
+	}
+	
+	swiper {
+		height: 100%;
+	}
+	
+	swiper-item {
+		text-align: center;
+	}
+	
+	swiper-item image {
+		width: 100%;
+	}
+	
+	.box {
+		width: 94%;
+		height: 100%;
+		display: inline-block;
+		box-shadow: 0 0 5px 1px rgba(0, 0, 0, .1);
+		border-radius: 5px;
+		img {
+			border-radius: 5px;
+			width: 100%;
+			height: 100%;
+		}
+	}
+	/*轮播*/
+	
+	.cate {
+		width: 100%;
+		padding: 37px 17px 16px 17px;
+		box-sizing: border-box;
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		.cate-li {
+			span {
+				display: block;
+				&:nth-child(1) {
+					width: 46px;
+					height: 46px;
+					margin: 0 auto;
+					img {
+						width: 100%;
+						height: 100%;
+					}
+				}
+				&:nth-child(2) {
+					text-align: center;
+					font-size: 12px;
+					color: #666666;
+					line-height: 26px;
+				}
+			}
+		}
+	}
+	/*海报*/
+	
+	.poster {
+		width: 100%;
+		height: 167px;
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
+	/*商品列表*/
+	
+	.headline {
+		text-align: center;
+		font-size: 15px;
+		color: #ff485a;
+		padding: 20px 0;
+		position: relative;
+		font-weight: bold;
+		&:before,
+		&:after {
+			content: "";
+			width: 32px;
+			height: 1px;
+			background: #ff485a;
+			display: block;
+			/*1.首先使伪类显示方式为块级元素*/
+			position: absolute;
+			/*2.通过相对定位的方式控制两侧内容的位置*/
+		}
+		&:before {
+			/*3.控制左侧横线的位置*/
+			top: 30px;
+			left: 120px;
+		}
+		&:after/*4.控制右侧横线的位置*/
+		{
+			top: 30px;
+			right: 120px;
+		}
+	}
+	
+	.product {
+		.product-list {
+			padding: 0 30px 0 10px;
+		}
+	}
 </style>
