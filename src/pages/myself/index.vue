72812bf15dbd7dc030d591e59c4b158d0401a9e0
unknown <1060556811@qq.com>
2019-02-23 17:47:27 +0800

    yxm

--- src/pages/myself/index.vue
+++ src/pages/myself/index.vue
@@ -1,206 +1,281 @@
 <template>
-  <div class="myself">
-    <div class="header">
-      <div class="userInfo">
-          <div class="avator">
-            <img src="/static/images/product-list.png">
-          </div>  
-          <div class="userName">
-            <p class="memberName">元淳面膜</p>
-            <p class="memberLv">合伙人</p>
-          </div>
-      </div>
-    </div>
-    <div class="menu">
-      <div class="list">
-        <p class="num">1</p>
-        <p class="name">平台券</p>
-      </div>
-      <div class="list">
-        <p class="num">1</p>
-        <p class="name">积分</p>
-      </div>
-      <div class="list">
-        <p class="num">13</p>
-        <p class="name">收藏</p>
-      </div>
-    </div>
-    <div class="order">
-      <div class="orderTitle">
-        <span>我的订单</span>
-        <span class="iconfont allOrer">&#xe72b;</span>
-        <span class="allOrer">全部订单</span>
-      </div>
-      <div class="orderItem">
-        <div class="orderList">
-          <p>1</p>
-          <p>待付款</p>
-        </div>
-        <div class="orderList">
-          <p>1</p>
-          <p>待发货</p>
-        </div>
-        <div class="orderList">
-          <p>1</p>
-          <p>待收货</p>
-        </div>
-        <div class="orderList">
-          <p>1</p>
-          <p>售后</p>
-        </div>
-      </div>
-    </div>
-    <div class="menuContain">
-      <div class="menuTitle">
-        我的服务
-      </div>
-      <div class="menuItem" v-for="(item,index) in menuItem" :key="item" :inde="index" @click="jump(item.jumpUrl)">
-        <div class="icon">
-          <img :src="item.icon">
-        </div>
-        <div class="menuname">{{item.menuName}}</div>
-      </div>
-    </div>
-  </div>
+	<div class="myself">
+		<div class="header">
+			<div class="userInfo">
+				<div class="avator">
+					<img src="/static/images/product-list.png">
+				</div>
+				<div class="userName">
+					<p class="memberName">元淳面膜</p>
+					<p class="memberLv">
+						<span>合伙人</span>
+						<span @click="jumpvipUp">点击升级</span>
+					</p>
+
+				</div>
+			</div>
+		</div>
+		<div class="menu">
+			<div class="list">
+				<p class="num">1</p>
+				<p class="name">平台券</p>
+			</div>
+			<div class="list">
+				<p class="num">1</p>
+				<p class="name">积分</p>
+			</div>
+			<div class="list">
+				<p class="num">13</p>
+				<p class="name">收藏</p>
+			</div>
+		</div>
+		<div class="order">
+			<div class="orderTitle">
+				<div class="allOrer-left">我的订单</div>
+				<div class="allOrer-right">
+					<span>全部订单</span>
+					<span class="iconfont">&#xe72b;</span>
+				</div>
+			
+			</div>
+			<div class="orderItem">
+				<div class="orderList">
+					<p>1</p>
+					<p>待付款</p>
+				</div>
+				<div class="orderList">
+					<p>1</p>
+					<p>待发货</p>
+				</div>
+				<div class="orderList">
+					<p>1</p>
+					<p>待收货</p>
+				</div>
+				<div class="orderList">
+					<p>1</p>
+					<p>售后</p>
+				</div>
+			</div>
+		</div>
+		<div class="menuContain">
+			<div class="menuTitle">
+				我的服务
+			</div>
+			<div class="menuItem" v-for="(item,index) in menuItem" :key="item" :inde="index" @click="jump(item.jumpUrl)">
+				<div class="icon">
+					<img :src="item.icon">
+				</div>
+				<div class="menuname">{{item.menuName}}</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				menuItem: [{
+						icon: '/static/images/icon3.png',
+						menuName: '我的合伙人',
+						jumpUrl: '../Partner/main'
+					},
+					{
+						icon: '/static/images/icon4.png',
+						menuName: '地址管理',
+						jumpUrl: '../address/main'
+					},
+					{
+						icon: '/static/images/kefu.png',
+						menuName: '联系客服',
+						jumpUrl: ''
+					},
+					{
+						icon: '/static/images/mingpian.png',
+						menuName: '个人名片',
+						jumpUrl: '../businessCard/main'
+					},
+					{
+						icon: '/static/images/tequan.png',
+						menuName: '我的特权',
+						jumpUrl: '../privilege/main'
+					}
+				]
+			}
+		},
 
+		components: {},
 
-export default {
-  data () {
-    return {
-      menuItem:[
-      {icon:'/static/images/icon3.png',menuName:'我的合伙人',jumpUrl:'../Partner/main'},
-      {icon:'/static/images/icon4.png',menuName:'地址管理',jumpUrl:'../address/main'},
-      {icon:'/static/images/kefu.png',menuName:'联系客服',jumpUrl:''},
-      {icon:'/static/images/mingpian.png',menuName:'个人名片',jumpUrl:'../businessCard/main'},
-      {icon:'/static/images/tequan.png',menuName:'我的特权',jumpUrl:'../privilege/main'}]
-    }
-  },
+		methods: {
+			jump(url) {
+				wx.navigateTo({
+					url: url,
+				})
+			},
+			jumpvipUp:function(){
+				wx.navigateTo({
+					url: "../vipUp/main"
+				})
+			}
+		},
 
-  components: {
-  },
+		created() {
 
-  methods: {
-   jump(url){
-    wx.navigateTo({
-      url:url,
-    })
-   }
-  },
-
-  created () {
-   
-  }
-}
+		}
+	}
 </script>
 
 <style scoped lang="less">
-img{
-  width: 100%;
-  height: 100%;
-  display: block;
-}
-.header{
-  height: 110px;
-  background:linear-gradient(to top right,#FF3D3A,#F56564);
-  width: 100%;
-  .userInfo{
-    display: flex;
-    padding: 40px 0 15px 25px;
-    box-sizing: border-box;
-    .avator{
-      width: 55px;
-      height: 55px;
-      border-radius: 50%;
-      overflow: hidden;
-    }
-    .userName{
-      // flex-grow: 1;
-      padding-left: 10px;
-      box-sizing: border-box;
-      color: #323735;
-      .memberName{
-        height: 30px;
-        line-height:30px;
-      }
-      .memberLv{
-        color:#fff;
-        background: #901414;
-        padding: 0 10px;
-        box-sizing: border-box;
-        height: 25px;
-        line-height: 25px;
-        border-radius: 12.5px;
-        font-size: 16px;
-      }
-    }
-  }
-}
-.menu{
-  display: flex;
-  justify-content: space-around;
-  text-align: center;
-  padding: 10px 0;
-  background: #F9F9F9;
-  .list{
-    color:#454545;
-    font-size: 16px;
-  }
-}
-.order{
-  .orderTitle{
-    height: 30px;
-    line-height:30px;
-    padding: 0 20px;
-    box-sizing: border-box;
-    font-size: 18px;
-  .allOrer{
-    float: right;
-    color:#B6B6B6;
-  }
-  }
-  .orderItem{
-    display: flex;
-    justify-content: space-around;
-    text-align: center;
-    font-size: 16px;
-    padding: 10px 0;
-    box-sizing: border-box;
-    .orderList{
-      p{
-        height: 25px;
-        line-height:25px;
-      }
-    }
-  }
-}
-.menuContain{
-  .menuTitle{
-    height: 40px;
-    line-height:40px;
-    padding-left: 10px;
-    box-sizing: border-box;
-  }
-  .menuItem{
-    display: flex;
-    padding-left: 10px;
-    width:100%;
-    height: 40px;
-    line-height:40px;
-    font-size: 16px;
-    .icon{
-      width: 24px;
-      height: 24px;
-      overflow: hidden;
-      margin-top: 9px;
-    }
-    .menuname{
-    padding-left: 8px;
-    flex-grow: 1;
-    border-top: 1px solid #EDEDED;
-    }
-  }
-}
+	img {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
+	
+	.header {
+		height: 110px;
+		background: linear-gradient(to top right, #FF3D3A, #F56564);
+		width: 100%;
+		.userInfo {
+			display: flex;
+			padding: 40px 0 15px 25px;
+			box-sizing: border-box;
+			.avator {
+				width: 50px;
+				height: 50px;
+				border-radius: 50%;
+				overflow: hidden;
+			}
+			.userName {
+				// flex-grow: 1;
+				padding-left: 10px;
+				box-sizing: border-box;
+				color: #323735;
+				.memberName {
+					height: 30px;
+					line-height: 30px;
+					font-size: 16px;
+					color: #3a3a3a;
+					font-weight: bold;
+				}
+				.memberLv {
+					display: flex;
+					align-items: center;
+					span {
+						display: block;
+						&:nth-child(1) {
+							color: #fff;
+							background: #901414;
+							padding: 0 10px;
+							box-sizing: border-box;
+							height: 18px;
+							line-height: 18px;
+							border-radius: 9px;
+							font-size: 12px;
+							text-align: center;
+							vertical-align: middle;
+						}
+						&:nth-child(2) {
+							display: inline-block;
+							font-size: 8px;
+							margin-left: 5px;
+							color: #FFFFFF;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	.menu {
+		display: flex;
+		justify-content: space-around;
+		text-align: center;
+		padding: 10px 0;
+		background: #fdfdfd;
+		.list {
+			color: #313131;
+			font-size: 15px;
+			.name {
+				margin-top: 10px;
+			}
+		}
+	}
+	
+	.order {
+		.orderTitle {
+			display: flex;
+			justify-content:space-between ;
+			align-items: center;
+			padding: 13px 13px 20px 13px;
+			
+			.allOrer-left{
+				  font-size: 15px;
+					color: #313131;
+					font-weight: bold;
+			}
+			.allOrer-right{
+					color: #acacac;
+					font-size: 14px;
+					display: inline-flex;
+					align-items: center;
+					span{
+						&:nth-child(2){
+							font-size: 12px;
+							margin-left: 2px;
+						}
+					}
+			}
+		}
+		.orderItem {
+			display: flex;
+			justify-content: space-around;
+			text-align: center;
+			font-size: 16px;
+			padding-bottom: 20px;
+			box-sizing: border-box;
+			.orderList {
+				p {
+					height: 25px;
+					line-height: 25px;
+					color: #313131;
+					font-size: 13px;
+				}
+			}
+		}
+	}
+	
+	.menuContain {
+		padding-bottom: 48px;
+		.menuTitle {
+			font-size: 15px;
+			color: #313131;
+			font-weight: bold;
+			box-sizing: border-box;
+			padding-left: 13px;
+			padding-bottom: 8px;
+		}
+		.menuItem {
+			display: flex;
+			padding-left: 10px;
+			width: 100%;
+			height: 40px;
+			line-height: 40px;
+			font-size: 16px;
+			border-top: 1px solid #EDEDED;
+			.icon {
+				width: 18px;
+				height: 18px;
+				overflow: hidden;
+				margin-top: 9px;
+			}
+			.menuname {
+				padding-left: 15px;
+				flex-grow: 1;
+				font-size: 11px;
+				color: #000000;
+				
+			}
+		}
+	}
 </style>
